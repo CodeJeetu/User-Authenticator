796c149e4a35bf645ac63f5dbe27665441f04e2e
CodeJeetu <coderjitendra1@gmail.com>
2024-07-16 20:17:12 +0530

    Changes Path

--- startApplication.bat
+++ startApplication.bat
@@ -4,7 +4,7 @@ REM Change directory to JDK 17's bin directory
 cd "C:\Program Files\Java\jdk-17\bin"
 
 REM RUN THE jar replace this path  E:\Spring_Project\User-Authenticatior\target to actual downloaded directory
-java.exe -XX:+UseG1GC -XX:+UseStringDeduplication -Xms512m -Xmx1024m -jar "E:\Spring_Project\User-Authenticatior\target\User-Authenticatior-Api.jar"
+java.exe -XX:+UseG1GC -XX:+UseStringDeduplication -Xms512m -Xmx1024m -jar "YOUR_CLONE_DIRECTORY\User-Authenticator\User-Authenticatior-Api.jar"
 
 REM Pause to keep the terminal window open after execution (optional)
 pause
